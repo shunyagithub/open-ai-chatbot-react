b81e9df0e6dc492dc24238c696097dec7d9dbcf2
Shunya Koide <56504519+shunyagithub@users.noreply.github.com>
2023-03-09 02:04:00 +0900

    Add open ai api

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,53 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import { useCallback, useEffect, useState } from "react";
-import type  {FC} from 'react'
-import "./App.css";
-import axios from 'axios'
-import { OpenAIApi, Configuration } from "openai";
+import { useCallback, useState } from 'react'
+import type { FC } from 'react'
+import './App.css'
+import { OpenAIApi, Configuration } from 'openai'
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const App: FC = () => {
+  const [input, setInput] = useState<string>('')
+  const [completedSentence, setCompletedSentence] = useState('')
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_OPEN_AI_API_KEY,
+  })
 
-const openai = new OpenAIApi(configuration)
-
-const App:FC = () => {
-  const [input, setInput] = useState<string>("");
-  const [completedSentence, setCompletedSentence] = useState("");
-
-  const model = 'text-davinci-002'
+  const openai = new OpenAIApi(configuration)
+  const model = 'gpt-3.5-turbo'
 
   const fetchData = async (input: string): Promise<string> => {
     const response = await openai.createChatCompletion({
       model,
-      messages: [{role: 'user', content: input}]
+      messages: [{ role: 'user', content: input }],
     })
-  console.log(response.data.choices[0].message?.content)
-    return response.data.choices[0].message?.content as string;
-  };
+    return response.data.choices[0].message?.content as string
+  }
 
-  const handleClick = useCallback(
-    async () => {
-      console.log('click')
-      try {
-        const completedSentence = await fetchData(input);
-        setCompletedSentence(completedSentence);
-      } catch (error) {
-        console.error('error', error);
-      }
-    },
-    [],
-  )
+  const handleClick = useCallback(async () => {
+    try {
+      const completedSentence = await fetchData(input)
+      setCompletedSentence(completedSentence)
+    } catch (error) {
+      console.error('error', error)
+    }
+  }, [])
 
-
-  useEffect(() => {
-    console.log('comp' , completedSentence)
-  }, [completedSentence])
-  
-  
   return (
     <div className="container">
-      <h2>Tell me something, and I`&apos;ll tell you more</h2>
+      <h2>Tell me something, and I&apos;ll tell you more</h2>
       <textarea
         value={input}
-        onChange={(event) => {setInput(event.target.value)}}
+        onChange={(event) => {
+          setInput(event.target.value)
+        }}
         rows={5}
         placeholder="Type in some words and I'll finish the rest..."
       />
-      <button className="button" onClick={handleClick}>Complete Sentence</button>
+      <button className="button" onClick={handleClick}>
+        Complete Sentence
+      </button>
       {completedSentence != null && <p>Completed sentence: {completedSentence}</p>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
