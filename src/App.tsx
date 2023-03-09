c2cb56e6fcb0f83703ac879bee100d3aa1145095
Shunya Koide <56504519+shunyagithub@users.noreply.github.com>
2023-03-09 19:19:24 +0900

    add chat function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import { useCallback, useState } from 'react'
+import { useEffect, useState } from 'react'
 import type { FC } from 'react'
 import './App.css'
-import { OpenAIApi, Configuration } from 'openai'
+import { OpenAIApi, Configuration, ChatCompletionRequestMessageRoleEnum } from 'openai'
+import type { ChatCompletionRequestMessage } from 'openai'
 
 const App: FC = () => {
   const [input, setInput] = useState<string>('')
-  const [completedSentence, setCompletedSentence] = useState('')
+  const [answer, setAnswer] = useState<string>('')
+  const [prevMessages, setPrevMessages] = useState<ChatCompletionRequestMessage[]>([])
+
   const configuration = new Configuration({
     apiKey: process.env.REACT_APP_OPEN_AI_API_KEY,
   })
@@ -17,19 +20,46 @@ const App: FC = () => {
   const fetchData = async (input: string): Promise<string> => {
     const response = await openai.createChatCompletion({
       model,
-      messages: [{ role: 'user', content: input }],
+      messages: [
+        {
+          role: ChatCompletionRequestMessageRoleEnum.System,
+          content: `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI`,
+        },
+        ...prevMessages,
+        { role: ChatCompletionRequestMessageRoleEnum.User, content: input },
+      ],
+      top_p: 0.5,
+      frequency_penalty: 0.5,
     })
     return response.data.choices[0].message?.content as string
   }
 
-  const handleClick = useCallback(async () => {
+  useEffect(() => {
+    const newMessages = [
+      {
+        role: ChatCompletionRequestMessageRoleEnum.User,
+        content: input, // 直前の質問内容
+      },
+      {
+        role: ChatCompletionRequestMessageRoleEnum.Assistant,
+        content: answer, // 直前の回答
+      },
+    ]
+
+    if (input !== '' && answer !== '') {
+      setPrevMessages([...prevMessages, ...newMessages])
+    }
+    setInput('')
+  }, [answer])
+
+  const handleClick = async (): Promise<void> => {
     try {
-      const completedSentence = await fetchData(input)
-      setCompletedSentence(completedSentence)
+      const res = await fetchData(input)
+      setAnswer(res)
     } catch (error) {
       console.error('error', error)
     }
-  }, [])
+  }
 
   return (
     <div className="container">
@@ -43,9 +73,17 @@ const App: FC = () => {
         placeholder="Type in some words and I'll finish the rest..."
       />
       <button className="button" onClick={handleClick}>
-        Complete Sentence
+        Chat
       </button>
-      {completedSentence != null && <p>Completed sentence: {completedSentence}</p>}
+      <ul>
+        {prevMessages?.map((messages, index) => {
+          return (
+            <li key={index}>
+              {messages.role}: {messages.content}
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
